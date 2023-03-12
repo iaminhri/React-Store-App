b05e467fe690501b0834c0990c759c035b1c001b
iaminhri <44377810+iaminhri@users.noreply.github.com>
2023-03-12 05:35:02 -0400

    Animation Added

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../Store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+    const [buttonHightlight, setButtonHighlight] = useState(false);
     const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const {items} = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
+
+    const btnClasses = `${classes.button} ${buttonHightlight ? classes.bump : ''}`;
+
+    useEffect( () => {
+        if(items.length === 0){
+            return;
+        }
+        setButtonHighlight(true);
+        const timer = setTimeout( () => {
+            setButtonHighlight(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClickCustom}>
+        <button className={btnClasses} onClick={props.onClickCustom}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
